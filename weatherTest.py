c9b8aee96b9a53ed85544b8f677373a92677e122
mosnyder <Morgan.Snyder@usiouxfalls.edu>
2018-07-27 14:20:41 -0500

    Fixed indent error for running the thing... maybe that was the issue?!?

--- weatherTest.py
+++ weatherTest.py
@@ -64,9 +64,9 @@ def test():
 
 #-----------------------------------------------------------------------
 
-    if __name__ == '__main__':
-        port = int(os.getenv('PORT', 5000))
-        app.run(debug=False, port = port, host='0.0.0.0')
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=False, port = port, host='0.0.0.0')
 
 
 
